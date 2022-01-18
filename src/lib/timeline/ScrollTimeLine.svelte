bfccaf69343eaf361819f4dd44a3c283193b178d
Gabriele Garlaschelli <gabriele.garlaschelli.photo@gmail.com>
2022-01-19 00:42:27 +0100

    scroll time line

--- src/lib/timeline/ScrollTimeLine.svelte
+++ src/lib/timeline/ScrollTimeLine.svelte
@@ -1,40 +1,174 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  let isMobile: boolean = false;
 
-  let titleElement: HTMLElement;
+  type Section = {
+    title: string
+  }
+
+  let isMobile: boolean = false;
+  let sections: Section[] = [
+    {
+      title: '10 apr, 2021'
+    },
+    {
+      title: 'Titolo 2'
+    },
+    {
+      title: 'Titolo 3'
+    },
+    {
+      title: 'Titolo 4'
+    },
+    {
+      title: 'Titolo 5'
+    },
+    {
+      title: 'Titolo 6'
+    },
+    {
+      title: 'Titolo 7'
+    },
+    {
+      title: 'Titolo 8'
+    },
+    {
+      title: 'Titolo 9'
+    },
+    {
+      title: 'Titolo 10'
+    },
+    {
+      title: 'Titolo 11'
+    },
+    {
+      title: 'Titolo 12'
+    },
+    {
+      title: 'Titolo 13'
+    },
+    {
+      title: 'Titolo 14'
+    },
+    {
+      title: 'Titolo 15'
+    },
+    {
+      title: 'Titolo 16'
+    },
+    {
+      title: 'Titolo 17'
+    },
+  ];
+
+  let titleElements: HTMLElement[] = [];
   onMount(() => {
-    titleElement.scrollIntoView({
-      block: 'center',
-    });
+    if(!!titleElements[0]) {
+      titleElements[0].scrollIntoView({
+        block: 'center',
+      });
+    }
   })
+
+
+  let scrollableDiv, titlesMobile, visibleIndex = 0, titlesMobileElement: HTMLElement,
+  function handleElemenScroll(event) {
+    let scrollValue: number = scrollableDiv.scrollTop
+    let scrollableDivHeight: number = scrollableDiv.offsetHeight
+    let titlesMobileHeight: number = titlesMobileElement.offsetHeight
+
+    let newVisibleIndex = Math.floor(scrollValue / scrollableDivHeight)
+    if(newVisibleIndex != visibleIndex) {
+      changeVisibleSection(newVisibleIndex, newVisibleIndex > visibleIndex)
+    }
+  }
+
+  function changeVisibleSection(index: number, top=true) {
+    visibleIndex = index
+
+    scrollTitlesToIndex(index)
+    if(top) {
+      scrollableDiv.scrollTop = index * scrollableDiv.offsetHeight
+    } else {
+      scrollableDiv.scrollTop = ((index + 1) * scrollableDiv.offsetHeight) - 1 + titlesMobileElement.offsetHeight
+    }
+  }
+
+  let titlesScrollableDiv: HTMLElement,
+    interval,
+    target
+
+  function scrollTitlesToIndex(index: number) {
+    if(!!titleElements[index]) {
+      const distanceFromParent = titleElements[index].offsetTop - titleElements[index].parentElement.offsetTop,
+        titleElementHeight = titleElements[index].offsetHeight,
+        parentHeight = titleElements[index].parentElement.offsetHeight
+
+      target = distanceFromParent - Math.floor((parentHeight / 2) - (titleElementHeight / 2))
+
+      if(!!interval) clearInterval(interval)
+      interval = setInterval(() => {
+        if(titlesScrollableDiv.scrollTop > target - 5 && titlesScrollableDiv.scrollTop < target + 5) {
+          clearInterval(interval)
+          titlesScrollableDiv.scrollTop = target
+        }
+        else if(titlesScrollableDiv.scrollTop > target && titlesScrollableDiv.scrollTop - target < 100) titlesScrollableDiv.scrollTop -= 5
+        else if(titlesScrollableDiv.scrollTop > target) titlesScrollableDiv.scrollTop -= 10
+        else if(titlesScrollableDiv.scrollTop < target && target - titlesScrollableDiv.scrollTop < 100) titlesScrollableDiv.scrollTop += 5
+        else if(titlesScrollableDiv.scrollTop < target ) titlesScrollableDiv.scrollTop += 10
+      }, 20)
+    }
+  }
+
+  $: cssVariables = Object.entries({
+      '--section-number': sections.length
+    }).filter(([key]) => key.startsWith('--'))
+    .reduce( (css, [key,value]) => {
+      return `${ css }${ key }: ${ value };`
+    }, '');
+
   import '$lib/common/tailwind.css';
 </script>
 
-<div class="timeline-container">
+<div class="timeline-container" style={cssVariables}>
   <div 
-    class="flex h-full"
-    class:flex-col={isMobile}
+    class="flex flex-col md:flex-row h-full"
   >
-    <div class="w-1/5 flex flex-col h-full-screen overflow-auto menu-container">
-      <div class="flex-none h-screen"></div>
-      <div class="font-sans text-2xl subpixel-antialiased font-bold tracking-wide" bind:this={titleElement}>
-        Header 1
-      </div>
-      <div>
-        Header 2
-      </div>
-      <div>
-        Header 4
-      </div>
-      <div>
-        Header 5
-      </div>
-      <div class="flex-none h-screen"></div>
+    <div class="w-1/5 flex-col h-full-screen menu-container overflow-auto hidden md:flex" bind:this={titlesScrollableDiv}>
+      <div class="flex-none h-1/2"></div>
+      {#each sections as section, index}
+        <div 
+          class="font-sans subpixel-antialiased tracking-wide transition-all cursor-pointer"
+          class:text-2xl={index == visibleIndex}
+          class:font-bold={index == visibleIndex}
+          bind:this={titleElements[index]}
+          on:click={() => changeVisibleSection(index)}
+        >
+          {section.title}
+        </div>
+      {/each }
+      <div class="flex-none h-1/2"></div>
     </div>
-    <div class="w-4/5 flex justify-center">
-      <div class="h-full">
-
+    <div class="flex md:hidden justify-center" bind:this={titlesMobileElement}>
+      <div 
+        class="font-sans subpixel-antialiased text-2xl font-bold tracking-wide transition-all cursor-pointer"
+      >
+        {sections[visibleIndex].title}
+      </div>
+    </div>
+    <div 
+      class="w-4/5 justify-center overflow-auto scrollable-div" 
+      on:scroll={handleElemenScroll} 
+      bind:this={scrollableDiv}
+    >
+      <div class="item-container">
+        {#each sections as section, index}
+          <div 
+            class="sticky top-0 contain-section transition-all"
+            class:opacity-0={index != visibleIndex}
+          >
+            {section.title}
+          </div>
+        {/each}
       </div>
     </div>
   </div>
@@ -49,10 +183,27 @@
   }
 
   .menu-container {
-    -ms-overflow-style: none;  /* Internet Explorer 10+ */
-    scrollbar-width: none;  /* Firefox */
+    -ms-overflow-style: none;
+    scrollbar-width: none;
   }
   .menu-container::-webkit-scrollbar { 
-    display: none;  /* Safari and Chrome */
+    display: none;
+  }
+
+  .scrollable-div {
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+  }
+  .scrollable-div::-webkit-scrollbar { 
+    display: none;
+  }
+
+  .contain-section {
+    height: var(--height);
+    max-width: var(--max-width);
+  }
+
+  .item-container {
+    height: calc(var(--height) * var(--section-number));
   }
 </style>
--- src/routes/timeline/ScrollTimeLine.svelte
+++ src/routes/timeline/ScrollTimeLine.svelte
@@ -6,8 +6,8 @@
 
 <div class="w-screen h-screen flex justify-center items-center">
   <ScrollTimeLine
-    --height="600px"
-    --width="600px"
+    --height="300px"
+    --width="800px"
     --max-width="100vw"
     --max-height="100vh"
   ></ScrollTimeLine>
