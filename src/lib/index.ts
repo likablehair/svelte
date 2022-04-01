98434e4cd82f6ce776cf7b61d7effd68716de3a6
Gabriele Garlaschelli <gabriele.garlaschelli.photo@gmail.com>
2022-04-01 16:46:31 +0200

    new version

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,31 @@
+// buttons
 export { default as Button } from './buttons/Button.svelte'
+// common
+export { default as Card } from './common/Card.svelte'
+export { default as Gesture } from './common/Gesture.svelte'
+export { default as IntersectionObserver } from './common/IntersectionObserver.svelte'
+// forms
+export { default as TextField } from './forms/TextField.svelte'
+// loaders
+export { default as Skeleton } from './forms/Skeleton.svelte'
+// media
+export { default as AttachmentDownloader } from './forms/AttachmentDownloader.svelte'
+export { default as Avatar } from './forms/Avatar.svelte'
+export { default as Carousel } from './forms/Carousel.svelte'
+export { default as DescriptiveAvatar } from './forms/DescriptiveAvatar.svelte'
+export { default as Icon } from './forms/Icon.svelte'
+export { default as Image } from './forms/Image.svelte'
+export { default as ImageGrid } from './forms/ImageGrid.svelte'
+// navigation
+export { default as BreadCrumb } from './navigation/BreadCrumb.svelte'
 export { default as HeaderMenu } from './navigation/HeaderMenu.svelte'
+export { default as Navigator } from './navigation/Navigator.svelte'
+export { default as TabSwitcher } from './navigation/TabSwitcher.svelte'
+// progress
+export { default as ProgressBar } from './navigation/ProgressBar.svelte'
+// shop
+export { default as ProductCard } from './shop/ProductCard.svelte'
+export { default as ProductGrid } from './shop/ProductCard.svelte'
+// timeline
+export { default as ScollTimeLine } from './shop/ScrollTimeLine.svelte'
+export { default as SimpleTimeLine } from './shop/SimpleTimeLine.svelte'
