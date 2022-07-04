626cbe98ff94e7e04491b1d73185be09782a3445
davide sonlieti <sonlietidavide@gmail.com>
2022-07-04 12:58:11 +0200

    COMP 23 textarea styling

--- src/lib/forms/Textarea.svelte
+++ src/lib/forms/Textarea.svelte
@@ -1,29 +1,91 @@
 <script lang="ts">
-  type VariantOptions = 'outlined' | 'boxed'
 
-  export let label: string = "label",
+  export let label: string = null,
+    placeholder: string = "placeholder",
+    fontFamily: string = "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    value: string = "",
+    resizable: boolean = true,
+    disabled: boolean = false,
+    readOnly: boolean = false,
+    maxLength: number = undefined,
+    textColor: string = "black",
+    border: string = "none",
+    borderRadius: string = "5px",
+    focusedBoxShadow: string = undefined,
+    backgroundColor: string = null,
+    padding: string = undefined,
+    paddingLeft: string = undefined,
+    paddingRight: string = undefined,
+    paddingBottom: string = undefined,
+    paddingTop: string = undefined,
+    fontSize: string = undefined,
+    labelFontSize: string = undefined,
+    labelColor: string = undefined,
     width: string = "100%",
     height: string = "100%";
 
     import { v4 as uuidv4 } from 'uuid';
 
-    let textAreaId: string = uuidv4(),
-      focused: boolean = false;
+    let textAreaId: string = uuidv4();
 
 </script>
 
 <div
-  style:width={width}
-  style:height={height}
   class="container"
 >
-  <label for={textAreaId}>{label}</label>
+  {#if label}
+    <label
+      for={textAreaId}
+      style:font-size={labelFontSize}
+      style:color={labelColor}
+    >
+      {label}
+    </label>
+  {/if}
   <textarea
-    style:width="100%"
-    style:height="100%"
-  ></textarea>
+    style:width={width}
+    style:height={height}
+    style:font-family={fontFamily}
+    style:resize={resizable?"both":"none"}
+    style:font-size={fontSize}
+    style:border-radius={borderRadius}
+    style:background-color={backgroundColor}
+    style:color={textColor}
+    style:padding={padding}
+    style:padding-left={paddingLeft}
+    style:padding-right={paddingRight}
+    style:padding-bottom={paddingBottom}
+    style:padding-top={paddingTop}
+    style:border={border}
+    style:--textarea-focus-box-shadow={focusedBoxShadow}
+    bind:value={value}
+    id={textAreaId}
+    placeholder={placeholder}
+    maxlength={maxLength}
+    disabled={disabled}
+    readonly={readOnly}
+    ></textarea>
 </div>
 
 
 <style>
+  textarea {
+    display: block;
+    transition: all 0.3s;
+  }
+
+  label {
+    display: block;
+    margin-bottom: 10px;
+    margin-left: 10px;
+  }
+
+  textarea:focus{
+    outline: none;
+    -webkit-box-shadow: var(--textarea-focus-box-shadow);
+    -moz-box-shadow: var(--textarea-focus-box-shadow);
+    -o-box-shadow: var(--textarea-focus-box-shadow);
+    box-shadow: var(--textarea-focus-box-shadow);
+  }
+
 </style>
--- src/routes/forms/Textarea.svelte
+++ src/routes/forms/Textarea.svelte
@@ -2,19 +2,33 @@
 
   import Textarea from '$lib/forms/Textarea.svelte';
 
-  let textValue = "my text"
+  let textValue = ""
 
   function handleInputChange(event) {
     console.log(event)
   }
-</script>
 
+</script>
 
 <div class="card-container">
   <Textarea
+    label="prova"
+    placeholder="testo..."
+    labelFontSize="20px"
+    labelColor="red"
     width = "500px"
     height = "200px"
+    bind:value={textValue}
+    resizable={true}
+    maxLength={200}
+    fontSize="14px"
+    borderRadius="4px"
+    backgroundColor="rgba(0,0,0,0.05)"
+    textColor="black"
+    padding="15px"
+    focusedBoxShadow="0px 0px 2px 1px #0063FF"
   ></Textarea>
+  {textValue}
 </div>
 
 <style>
