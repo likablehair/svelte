63248b929a3cf83d9fb7ac910bf74ef1a71e86d1
filippopassalacqua <filippo.passalacqua.13@gmai.com>
2023-09-26 11:14:59 +0200

    COMP-35 fix date picker mask

--- src/routes/docs/components/simple-components/DatePickerTextField/+page.svelte
+++ src/routes/docs/components/simple-components/DatePickerTextField/+page.svelte
@@ -2,13 +2,20 @@
   import ComponentSubtitle from "../../../ComponentSubtitle.svelte";
   import PropsViewer from "../../PropsViewer.svelte";
   import DatePickerTextField from "$lib/components/simple/dates/DatePickerTextField.svelte";
+
+  let date: Date | undefined = undefined
+
+  setTimeout(() => {
+    date = new Date('2024-01-02')
+    console.log(date)
+  }, 2000);
 </script>
 
 <h1>DatePickerTextField</h1>
 <ComponentSubtitle>If you want a date, you'll have a date.</ComponentSubtitle>
 <h2>Example</h2>
 <div class="example">
-  <DatePickerTextField></DatePickerTextField>
+  <DatePickerTextField bind:selectedDate={date} />
 </div>
 <h2>Props</h2>
 <PropsViewer
@@ -28,7 +35,7 @@
     //   description: 'The max width of the outer element'
     // },
   ]}
-></PropsViewer>
+/>
 <h2>Slots</h2>
 <h2>Events</h2>
 
