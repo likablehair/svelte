46184f6276be5e77a099738d3dda6ab3864c079d
Giacomo Folli <giacomofolli01@gmail.com>
2024-01-13 04:57:23 +0100

    date as prop

--- src/lib/components/composed/forms/DatePickerTextField.svelte
+++ src/lib/components/composed/forms/DatePickerTextField.svelte
@@ -32,7 +32,10 @@
     visibleYear: number | undefined = undefined,
     selectedDate: Date | undefined = undefined,
     placeholder: string | undefined = undefined,
-    mobileDialog: boolean = true
+    mobileDialog: boolean = true,
+    maxYearInRange: number = 1970,
+    minYearInRange: number = 2100
+
 
   let activator: HTMLElement,
     refreshPosition = false,
@@ -53,8 +56,8 @@
       blocks: {
         yyyy: {
           mask: IMask.MaskedRange,
-          from: 1970,
-          to: 2100
+          from: minYearInRange,
+          to: maxYearInRange
         },
         MM: {
           mask: IMask.MaskedRange,
