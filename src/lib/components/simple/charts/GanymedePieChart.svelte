ab2cad763abba50f816e1a842975cd3064739940
Fabio Rossi <62110653+FabioRossi-99@users.noreply.github.com>
2024-04-04 16:13:40 +0200

    Comp 45 ganymede pie chart (#42)
    * COMP-44 GanymedeBarChart
    
    * COMP-45 GanymedePieChart
    
    * COMP-45 Removed GanymedeBarChart from comp GanymedePieChart

---
+++ src/lib/components/simple/charts/GanymedePieChart.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import { Pie } from "svelte-chartjs";
+  import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    LinearScale,
+    PointElement,
+    CategoryScale,
+    ArcElement,
+  } from "chart.js";
+  import type { ComponentProps } from "svelte";
+
+  ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    ArcElement,
+    LinearScale,
+    PointElement,
+    CategoryScale
+  );
+
+  export let data: {
+    labels: string[];
+    datasets: {
+      label: string;
+      data: number[];
+      backgroundColor?: string[];
+      borderColor?: string;
+      hoverBackgroundColor?: string[];
+      hoverOffset?: number;
+    }[];
+    options?: {
+      title: {
+        display: boolean;
+        text: string;
+      };
+    };
+  } = {
+    labels: [],
+    datasets: [],
+    options: {
+      title: {
+        display: false,
+        text: "",
+      },
+    },
+  },
+  showLegend: boolean = true;
+
+  let chartOptions: ComponentProps<Pie>['options']
+  $: chartOptions = {
+      plugins: {
+        legend: {
+          display: showLegend
+        }
+      }
+    }
+</script>
+
+<Pie bind:data options={chartOptions}/>
