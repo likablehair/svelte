3bb6b246bddffc4ad1002b0e6d78edd0036ed58f
Fabio Rossi <62110653+FabioRossi-99@users.noreply.github.com>
2024-04-09 16:24:39 +0200

    COMP-44 GanymedeBarChart (#41)
    * COMP-44 GanymedeBarChart
    
    * COMP-44 Ganymede Bar chart new style

---
+++ src/lib/components/simple/charts/GanymedeBarChart.svelte
@@ -0,0 +1,168 @@
+<script lang="ts">
+  import { Bar } from 'svelte-chartjs'
+  import { theme } from '$lib';
+  import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    LineElement,
+    LinearScale,
+    PointElement,
+    CategoryScale,
+    BarElement,
+  } from 'chart.js';
+  import { onMount, type ComponentProps } from 'svelte';
+
+  ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    LineElement,
+    BarElement,
+    LinearScale,
+    PointElement,
+    CategoryScale
+  );
+
+
+  export let data: {
+      labels: string[],
+      datasets: {
+        label: string,
+        data: number[],
+        backgroundColor?: string,
+        borderColor?: string,
+        hoverBackgroundColor?: string[]
+        tension?: number,
+      }[]
+    } = {
+      labels: [],
+      datasets: []
+    },
+    horizontal: boolean = false,
+    responsive: boolean = true,
+    maintainAspectRatio: boolean = true,
+    showLegend: boolean = true,
+    showYTicks: boolean = false,
+    showXTicks: boolean = false,
+    displayYGrid: boolean = true,
+    lineWidth: number = 1,
+    enableZoom: boolean = true,
+    resetZoom: boolean = false
+
+  let mounted: boolean = false,
+    zoomMounted: boolean = false
+
+  onMount(() => {
+    mounted = true
+  })
+
+  let rgbTooltipColor: string | undefined = undefined
+  let rgbTooltipBackgroundColor: string | undefined = undefined
+  let rgbBackgroundColor: string | undefined = undefined
+
+  $: rgbTooltipColor = $theme.colors?.[$theme.active]['dark']['primary']['300']
+  $: rgbTooltipBackgroundColor = $theme.colors?.[$theme.active]['dark']['primary']['900']
+  $: rgbBackgroundColor = $theme.colors?.[$theme.active]['dark']['background']['200']
+
+  $: finalTooltipColor = !!rgbTooltipColor ? `rgb(${rgbTooltipColor}, .8)` : undefined
+  $: finalTooltipBackgroundColor = !!rgbTooltipBackgroundColor ? `rgb(${rgbTooltipBackgroundColor})` : undefined
+  $: finalBackgroundColor = !!rgbBackgroundColor ? `rgb(${rgbBackgroundColor}, .3)` : undefined  
+
+
+  let chart: ComponentProps<Bar>['chart']
+  $: if(!!chart && !!enableZoom && !!resetZoom) {
+    chart.resetZoom()
+    resetZoom = false
+  }
+
+  $: if(enableZoom && mounted) {
+    import('chartjs-plugin-zoom').then(({ default: zoomPlugin }) => {
+      ChartJS.register(zoomPlugin)
+      zoomMounted = true
+    })
+  }
+
+  let chartOptions: ComponentProps<Bar>['options']
+  $: chartOptions = {
+      barPercentage: 0.9,
+      borderRadius: 2,
+      categoryPercentage: 0.3,
+      indexAxis: horizontal ? 'y' : 'x',
+      responsive: responsive,
+      maintainAspectRatio: maintainAspectRatio,
+      plugins: {
+        tooltip: {
+          displayColors: false,
+          titleColor: finalTooltipColor,
+          backgroundColor: finalTooltipBackgroundColor,
+          titleFont: {
+            size: 14
+          },
+          bodyFont: {
+            size: 14,
+            weight: "bold"
+          }
+        },
+        legend: {
+          display: showLegend
+        },
+        zoom: {
+          pan: {
+            enabled: true,
+            mode: 'x',
+            modifierKey: 'ctrl',
+          },
+          zoom: {
+            drag: {
+              enabled: true
+            },
+            mode: 'x',
+          },
+        }
+      },
+      interaction: {
+        intersect: false,
+      },
+      scales: {
+        x: {
+          display: true,
+          title: {
+            display: true
+          },
+          grid: {
+            display: false
+          },
+          border: {
+            display: false
+          },
+          ticks: {
+            display: showYTicks
+          }
+        },
+        y: {
+          display: displayYGrid,
+          title: {
+          },
+          grid: {
+            lineWidth: lineWidth,
+            color: finalBackgroundColor
+          },
+          border: {
+            dash: [10,10],
+            display: false
+          },
+          ticks: {
+            display: showXTicks
+          }
+        }
+      }
+    }
+</script>
+
+<Bar
+  bind:data={data}
+  options={chartOptions}
+  bind:chart={chart}
+></Bar>
---
+++ src/routes/docs/components/simple-components/GanymedeBarChart/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import ComponentSubtitle from "../../../ComponentSubtitle.svelte";
+  import PropsViewer from "../../PropsViewer.svelte";
+  import GanymedeBarChart from "$lib/components/simple/charts/GanymedeBarChart.svelte";
+  import Button from "$lib/components/simple/buttons/Button.svelte";
+
+  let resetZoom: boolean = false
+</script>
+
+<h1>GanymedeBarChart</h1>
+<ComponentSubtitle>Ganymede bar chart.</ComponentSubtitle>
+<h2>Example</h2>
+<div class="example">
+  <GanymedeBarChart
+    data={{
+      labels: ['Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dec'],
+      datasets: [
+        {
+          label: 'Vendite',
+          data: [20, 30, 20, 40, 50, 10, 60, 20, 30, 20, 80, 100],
+          backgroundColor: '#0891b2',
+          borderColor: '#7dd3fc',
+          tension: 0.3
+        }, {
+          label: 'Acquisti',
+          data: [60, 20, 30, 20, 80, 100, 20, 30, 20, 40, 50, 10],
+          backgroundColor: '#115e59',
+          borderColor: '#0d9488',
+          tension: 0.3
+        },
+      ]
+    }}
+    showLegend={false}
+    showXTicks={true}
+    showYTicks={true}
+    enableZoom={true}
+    lineWidth={0}
+    bind:resetZoom={resetZoom}
+  ></GanymedeBarChart>
+  <Button
+    on:click={() => { resetZoom = true }}
+  >Reset zoom</Button>
+</div>
+<h2>Props</h2>
+<PropsViewer
+  props={[]}
+  styleProps={[]}
+></PropsViewer>
+<h2>Slots</h2>
+<h2>Events</h2>
+
+<style>
+  .example {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    margin-bottom: 20px;
+  }
+</style>
