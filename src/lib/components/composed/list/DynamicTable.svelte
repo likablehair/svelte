c114e2c4fe3912aac667bb5e6ad4c5efd06aab4e
vincenzo di sieno <disienovincenzo@gmail.com>
2025-04-30 15:28:06 +0200

    fix quick filter

--- src/lib/components/composed/list/DynamicTable.svelte
+++ src/lib/components/composed/list/DynamicTable.svelte
@@ -2193,6 +2193,7 @@
                   : quickFilterActive.description,
                 multiple: true,
               }}
+              --autocomplete-border-radius= 0.5rem
               --autocomplete-border="1px solid rgb(var(--global-color-background-500))"
               --autocomplete-focus-box-shadow="0 0 0 2px rgb(var(--global-color-primary-500))"
             />
