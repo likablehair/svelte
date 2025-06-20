e5f98366547d2ef439a227c1b18f09e5011bd1d8
Gabriele Garlaschelli <gabriele.garlaschelli@likablehair.it>
2025-06-20 15:52:46 +0200

    chore: update version to 4.0.11 and enhance TabSwitcher component with dynamic tabs

--- src/lib/components/simple/navigation/TabSwitcher.svelte
+++ src/lib/components/simple/navigation/TabSwitcher.svelte
@@ -9,7 +9,6 @@
 <script lang="ts">
   import { onMount, type Snippet } from "svelte";
   import './TabSwitcher.css'
-  import { BROWSER } from 'esm-env';
   import Icon from "../media/Icon.svelte";
 
   interface Props {
@@ -44,29 +43,16 @@
     mandatory = true,
     class: clazz = {},
     ontabClick,
-    ontabKeypress,
     appendSnippet,
   }: Props = $props();
 
   let tabButtons: Record<string, HTMLElement> = {};
   onMount(() => {
     if (mandatory && !selected && tabs.length > 0) selected = tabs[0].name;
-
-    if (selected) {
-      setBookmarkPosition();
-    }
   });
-
-  $effect(() => {
-    setBookmarkPosition();
-  });
-
-  let bookmarkWidth = $state(0),
-    bookmarkLeft = $state(0);
 
   function handleTabClick(clickedTab: Tab, nativeEvent: MouseEvent) {
     selected = clickedTab.name;
-    setBookmarkPosition();
     if(ontabClick) {
       ontabClick({
         detail: {
@@ -76,53 +62,23 @@
       })
     }
   }
-
-  function handleTabKeypress(clickedTab: Tab, nativeEvent: KeyboardEvent) {
-    selected = clickedTab.name;
-    setBookmarkPosition();
-    if(ontabKeypress) {
-      ontabKeypress({
-        detail: {
-          nativeEvent,
-          tab: clickedTab
-        }
-      })
-    }
-  }
-
-  function setBookmarkPosition() {
-    if(BROWSER) {
-      let tabButton: HTMLElement | undefined = selected
-        ? tabButtons[selected]
-        : undefined;
-  
-      if (tabButton) {
-        bookmarkWidth = tabButton.offsetWidth - 10;
-        bookmarkLeft = tabButton.offsetLeft + 5;
-      }
-    }
-  }
-
-  $effect(() => { if(!!selected) setBookmarkPosition() }) 
 </script>
 
 <div
   class="{clazz.container || ''} tabs-container"
 >
   {#each tabs as tab}
-    <div
-      role="presentation"
+    <button
       class:selected-tab={tab.name == selected}
       class="tab-label {clazz.tabs || ''} {tab.name == selected ? clazz.selected || '' : ''}"
       onclick={(event) => handleTabClick(tab, event)}
-      onkeypress={(event) => handleTabKeypress(tab, event)}
       bind:this={tabButtons[tab.name]}
     >
       {#if !!tab.icon}
         <Icon name={tab.icon}></Icon>
       {/if}
       {tab.label}
-    </div>
+    </button>
   {/each}
   {#if appendSnippet}
     <div
@@ -133,11 +89,6 @@
       {@render appendSnippet()}
     </div>
   {/if}
-  <span
-    style:left={bookmarkLeft + "px"}
-    style:width={bookmarkWidth + "px"}
-    class="{clazz.bookmark || ''} bookmark"
-  ></span>
   <span
     class="{clazz.guide || ''} horizontal-guide"
   ></span>
@@ -159,6 +110,21 @@
     );
   }
 
+  button {
+    background: none;
+    border: none;
+    padding: 0;
+    margin: 0;
+    color: inherit;
+    font: inherit;
+    cursor: pointer;
+    outline: none;  
+  }
+
+  button:focus {
+    outline: none;
+  }
+
   .selected-tab {
     color: var(
       --tab-switcher-selected-color,
@@ -166,9 +132,21 @@
     );
   }
 
+  .selected-tab::after {
+    content: '';
+    position: absolute;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    height: 2px;
+    background-color: var(
+      --tab-switcher-bookmark-color,
+      var(--tab-switcher-default-bookmark-color)
+    );
+  }
+
   .horizontal-guide {
     position: absolute;
-    z-index: 5;
     bottom: 0px;
     height: 1px;
     background-color: var(
@@ -188,18 +166,6 @@
     -webkit-tap-highlight-color: rgba(0,0,0,0);
     cursor: pointer;
     padding: 8px;
-  }
-
-  .bookmark {
-    position: absolute;
-    bottom: 0px;
-    height: 2px;
-    border-radius: 0.125rem;
-    z-index: 10;
-    background-color: var(
-      --tab-switcher-bookmark-color,
-      var(--tab-switcher-default-bookmark-color)
-    );
-    transition: left 400ms, width 400ms;
+    position: relative;
   }
 </style>
