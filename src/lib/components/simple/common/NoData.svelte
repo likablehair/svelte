9c7e62a2444be2754b753e58d1b666033c739dec
Gabriele Garlaschelli <gabriele.garlaschelli@likablehair.it>
2025-09-04 16:33:48 +0200

    no data refactor

--- src/lib/components/simple/common/NoData.svelte
+++ src/lib/components/simple/common/NoData.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-    import Icon from "../media/Icon.svelte";
+  import '../../../css/main.css'
+  import './NoData.css'
+  import Icon from "../media/Icon.svelte";
 
   interface Props {
     lang?: 'it' | 'en',
@@ -16,7 +18,26 @@
   }: Props = $props()
 </script>
 
-<div>
-  <Icon name={iconName} ></Icon>
-  <span class={clazz}>{noItemsText}</span>
+<div class="no-data-container">
+  <Icon name={iconName} --icon-default-size="40px"></Icon>
+  <span class="no-data-text {clazz || ''}">{noItemsText}</span>
 </div>
+
+<style>
+  .no-data-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: var(
+      --no-data-gap, 
+      var(--no-data-default-gap)
+    );
+    opacity: 50%;
+  }
+
+  .no-data-text {
+    font-size: 12px;
+    opacity: 50%;
+  }
+</style>
