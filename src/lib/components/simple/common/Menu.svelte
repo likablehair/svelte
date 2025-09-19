4aab237d8196990080c96d5607c838589d944409
cristianrizzo <cristian.rizzo@likablehair.it>
2025-09-19 12:08:19 +0200

    ADDIBOX-756 fix menu autocomplete (quickFilters) (#111)
    * fix: prevent event propagation on close controller click
    
    * refactor: use $state for menuOpened and activators in Menu component

--- src/lib/components/simple/common/Menu.svelte
+++ src/lib/components/simple/common/Menu.svelte
@@ -325,7 +325,8 @@
     return getPositionedAncestor(elem.parentElement)
   }
 
-  function handleCloseControllerClick() {
+  function handleCloseControllerClick(event: MouseEvent) {
+    event.stopPropagation()
     open = false
   }
 
@@ -406,7 +407,6 @@
   data-opening-id={openingId}
   data-uid={currentUid}
   onclick={handleCloseControllerClick}
-  onkeypress={handleCloseControllerClick}
   bind:this={closeController}
   role="presentation"
   tabindex="-1"
